0e9cf916536c19655bcef6ec1966a6f10a6713e3
N-Hosana <hosanah08@gmail.com>
2026-01-21 22:28:15 +0200

    NestJs

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,21 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from '../users/entities/user.entity';
-import { Route } from '../routes/entities/route.entity';
-import { RouteStop } from '../routes/entities/stop.entity';
-import { Bus } from '../buses/entities/bus.entity';
-import { TrackingRecord } from '../tracking/entities/tracking.entity';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: String(process.env.DB_USERNAME),
-  password: String(process.env.DB_PASSWORD),
-  database: String(process.env.DB_NAME),
-  entities: [User, Route, RouteStop, Bus, TrackingRecord],
-  synchronize: false,
-  dropSchema: false,
+  url: process.env.DATABASE_URL,
+  autoLoadEntities: true,
+  synchronize: true,
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
 };
